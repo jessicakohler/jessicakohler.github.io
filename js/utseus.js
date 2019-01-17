f8a0597d76b6b14d036500f0bd870e6a5b01de01
jessicakohler <jessica@MacBook-Air-de-Jessica.local>
2019-01-17 14:00:41 +0800

    Delete website v1

--- js/utseus.js
+++
@@ -1,8 +0,0 @@
-function dostuff(theForm){
-	var shuid = theForm.shuid.value.toUpperCase();
-	var passport = theForm.passport.value.toUpperCase();
-	var thepass = shuid + passport;
-	var thepage = md5(thepass) + ".html";
-	window.location.replace(thepage);
-	return false;
-}
